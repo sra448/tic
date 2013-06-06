9a657548426acb6347fbcd8dd03ffdac4f22362f
Florian Unternaehrer <justus.jonas@gmail.com>
2013-06-06 19:24:13 +0200

    inital commit, setup jasmine

---
+++ spec/tiqSpec.js
@@ -0,0 +1,11 @@
+describe("tiq, a functional approach to dating:", function() {
+
+  describe('some common functions', function() {
+
+    it("has tests", function() {
+      expect(010).toEqual(8);
+    });
+
+  });
+
+});
