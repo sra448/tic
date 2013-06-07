5b96575a0a624c190b4df1985bacded14ddb3cff
Florian Unternaehrer <justus.jonas@gmail.com>
2013-06-07 11:36:33 +0200

    do you really wanna hurt me?

--- spec/tiqSpec.js
+++ spec/tiqSpec.js
@@ -1,11 +1,21 @@
-describe("tiq, a functional approach to dating:", function() {
+describe('tiq.parse', function() {
 
-  describe('some common functions', function() {
+  it("returns a date object of the current date, given an empty string", function() {
+    expect(tiq.parse("")).toEqual(new Date());
+  });
 
-    it("has tests", function() {
-      expect(010).toEqual(8);
-    });
+  it("returns a date object, given a date string", function() {
+    expect(tiq.parse("Thu Jun 06 2013 19:40:02 GMT+0200 (CEST)")).toEqual(new Date("Thu Jun 06 2013 19:40:02 GMT+0200 (CEST)"));
+  });
 
+  it("returns a date object with ", function() {
+    expect(tiq.parse("06.06.2013")).toEqual(new Date("Thu Jun 06 2013 00:00:00 GMT+0200 (CEST)"));
+    expect(tiq.parse("6.6.2013")).toEqual(new Date("Thu Jun 06 2013 00:00:00 GMT+0200 (CEST)"));
+    expect(tiq.parse("20.6.2013")).toEqual(new Date("Thu Jun 20 2013 00:00:00 GMT+0200 (CEST)"));
+  });
+
+  it("returns a date object, given a date strding", function() {
+    expect(tiq.parse("Thu Jun 06 2013 19:40:02 GMT+0200 (CEST)")).toEqual(new Date("Thu Jun 06 2013 19:40:02 GMT+0200 (CEST)"));
   });
 
 });
---
+++ tiq.js
@@ -0,0 +1,15 @@
+var tiq = (function() {
+
+  var dateRegex = /dasdf/;
+
+  return {
+
+    parse: function(str) {
+      if (str === "") {
+        return new Date();
+      }
+    }
+
+  };
+
+})();
